119aae0aa2b6dbdd9caf206910364408d3ec57eb
JINJIALEI <jinjialei@JINJIALEIdeMBP.lan>
2021-09-27 15:05:11 +0800

    修改

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $a = \App\Models\SalesOrder::with('salesOrderBatches', 'receivePaymentBatches')->get();
+    $res = $a->map(function ($item){
+        $salesOrder = \App\Models\SalesOrder::find($item->id);
+        $shipment_amount = $item->salesOrderBatches->sum('amount');
+        $received_amount = $item->receivePaymentBatches->sum('amount');
+        $salesOrder->shipment_amount = $shipment_amount;
+        $salesOrder->received_amount = $received_amount;
+
+        if($salesOrder->amount == 0){
+            $salesOrder->is_shipment = false;
+            $salesOrder->is_received = false;
+        }else{
+            $salesOrder->is_shipment = $shipment_amount >= $salesOrder->amount;
+            $salesOrder->is_received = $received_amount >= $salesOrder->amount;
+        }
+        
+        $salesOrder->save();
+
+        return $item;
+    });
+
     return view('links');
 });
 
